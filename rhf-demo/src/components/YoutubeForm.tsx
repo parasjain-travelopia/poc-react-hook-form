86365dd41ec87fa3410ed5a896d1872057bab9f7
parasjain-travelopia <147372590+parasjain-travelopia@users.noreply.github.com>
2025-02-05 10:27:07 +0530

    register from useForm

--- rhf-demo/src/components/YoutubeForm.tsx
+++ rhf-demo/src/components/YoutubeForm.tsx
@@ -1,17 +1,32 @@
 /** @format */
 
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
 export const YoutubeForm = () => {
+  const form = useForm();
+  const { register, control } = form;
+  // const { name, ref, onChange, onBlur } = register("username");
+
   return (
     <div>
       <form>
         <label htmlFor='username'> Username</label>
-        <input type='text' id='username' name='username' />
+        <input type='text' id='username' {...register("username")} />
         <label htmlFor='email'> Email</label>
-        <input type='email' id='email' name='email' />
+        <input type='email' id='email' {...register("email")} />
         <label htmlFor='channel'> Channel</label>
-        <input type='text' id='channel' name='channel' />
+        <input type='text' id='channel' {...register("channel")} />
         <button>Submit</button>
       </form>
+      <DevTool control={control} />
     </div>
   );
 };
+
+// {
+//   values ;{ ...}
+//   visited: {.. }
+//   errors: {...}
+//   isValird:boolean
+// }
