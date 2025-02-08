8eed3f1de8a8728aad1e231d60a478accf756dd1
parasjain-travelopia <147372590+parasjain-travelopia@users.noreply.github.com>
2025-02-08 19:43:32 +0530

    Form submission

--- rhf-demo/src/components/YoutubeForm.tsx
+++ rhf-demo/src/components/YoutubeForm.tsx
@@ -3,14 +3,23 @@
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
+export type FormType = {
+  username: string;
+  email: string;
+  channel: string;
+};
+
 export const YoutubeForm = () => {
-  const form = useForm();
-  const { register, control } = form;
+  const form = useForm<FormType>();
+  const { register, control, handleSubmit } = form;
   // const { name, ref, onChange, onBlur } = register("username");
 
+  const onSubmit = (data: FormType) => {
+    console.log(data);
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor='username'> Username</label>
         <input type='text' id='username' {...register("username")} />
         <label htmlFor='email'> Email</label>
